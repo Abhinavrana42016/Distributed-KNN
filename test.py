6bf2d307f62a2bc3757a129124760dc286762676
Abhinav Rana <abhinavrana42016@gmail.com>
2018-03-16 17:24:55 +0530

    Initial Commit

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-fh = open("InterestedObjects")
-IOSet = list()
-for lines in fh:
-    IOSet.append(lines.rstrip())
-print(IOSet)
+# fh = open("InterestedObjects")
+# IOSet = list()
+# for lines in fh:
+#     IOSet.append(lines.rstrip())
+# print(IOSet)
+#
+
+
+d = [{"a": 1,"b":2}]
+for i in range(10):
+    print(i)
